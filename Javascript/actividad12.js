6099fa07b5df75ad661543e67ff0115806265fa6
GusV208 <130870232+GusV208@users.noreply.github.com>
2023-05-28 22:14:19 -0300

    Add files via upload

---
+++ Javascript/actividad12.js
@@ -0,0 +1,86 @@
+//Enlace con la página información.html para mostrar los resultados.
+
+
+// var topping="Ingrese el topping"
+// var precio=10
+// var precio2=15
+// var precio3=25
+// var precioFinal=(topping + helado)
+// var helado=100
+
+// if (topping=="Oreo") {
+//     console.log("El precio es: $"+precio);
+// }else if(topping=="KitKat") {
+//     console.log("El precio es: $"+precio2);
+// }else if(topping=="Kinder") {
+//     console.log("El precio es: $"+precio3);
+// }else {
+//     console.log("No tenemos este topping");
+// }
+
+// console.log("El helado cuesta $" + precioFinal)
+
+
+
+
+
+// var menu=prompt("Que desea ordenar: Carne, Pescado o Verdura")
+
+// switch(menu){
+//     case "Carne":
+//         alert("Le recomendamos acompañar con vino tinto");
+//         break;
+//     case "Pescado":
+//         alert("Le recomendamos acompañar con vino blanco");
+//         break;
+//     case "Verdura":
+//         alert("Le recomendamos acompañar con agua");
+//         break;
+//     default:
+//         alert("Elija Carne, Pescado o Verdura");
+//         break;            
+// }
+
+
+
+
+
+// var nombres=["Diego", "Anabel", "Esteban", "Sergio"]
+
+// for(let i=0;i<nombres.length;i++){
+//     console.log("El nombre es: " + nombres[i]);
+//     nombres.push("Gustavo");
+//     console.log("El nombre es: " + nombres[i]);    
+// }
+
+
+
+
+// var numero=11;
+
+// while(numero<11){
+//     console.log("numero más chico que 11")
+//     numero=numero-1;
+// }
+
+
+
+//Actividad 12 en página información.html
+
+
+var saludo=prompt("Ingrese su nombre: ")
+
+if (saludo==""){
+    alert("Ingrese su nombre");   
+}else{
+    alert("Bienvenido " + saludo)
+}
+
+// function saludo(nombre){
+//     var saludo=prompt("Ingrese su nombre: ")
+//     if (saludo==""){
+//         alert("Ingrese su nombre");   
+//     }else{
+//         alert("Bienvenido " + saludo)
+//     }
+// }
---
+++ Javascript/script.js
@@ -0,0 +1,34 @@
+//Actividad 10
+//Sólo lo enlace con la página index.html para mostrar los resultados.
+
+// var nombre="Gustavo";
+// var apellido="Valdez";
+
+// console.log(nombre + " " + apellido)
+
+
+// var numero1=3
+// var numero2=4
+// var suma=numero1+numero2
+
+// console.log("El resultado de la sumas es " + suma)
+
+
+// var numero3=suma + 5
+
+// console.log("El resultado de la suma es " + numero3)
+
+
+
+
+
+//Trabajo Final
+
+var edad=Number(prompt("Ingrese su edad: "))
+
+if (edad>=18){
+    alert("Ingresando")
+}else{ (edad<18)
+    alert("Ingreso bajo su responsabilidad")
+}
+
